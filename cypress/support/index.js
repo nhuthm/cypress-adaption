e4ccdb8f2b534ca9779d7655b0935c8287674b57
Nhut Huynh <huynhminhnhut@gmail.com>
2021-03-22 14:55:29 +0700

    chore: finish configuration cucumber into Cypress project

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,14 @@ import './commands'
 require('cypress-xpath')
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+module.exports = (on, config) => {
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+}
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = (on, config) => {
+    on('file:preprocessor', cucumber())
+}
